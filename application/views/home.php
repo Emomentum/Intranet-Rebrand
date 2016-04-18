353956c3303ad7c2375ab886b1b48acbc55ac97b
Emomentum <development.team@emomentum-interactive.com>
2016-04-18 15:19:52 +0300

    Update news feeds and colors
    Update news feeds and colors

--- application/views/home.php
+++ application/views/home.php
@@ -2,6 +2,107 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/custom.css">
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jssor.slider.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" />
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/script.js"></script>
+      <script>
+        jssor_1_slider_init = function() {
+            
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:1200,$Opacity:2}
+            ];
+            
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 358);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            //responsive code end
+        };
+    </script>
+
+    <style>
+        
+        /* jssor slider bullet navigator skin 05 css */
+        /*
+        .jssorb05 div           (normal)
+        .jssorb05 div:hover     (normal mouseover)
+        .jssorb05 .av           (active)
+        .jssorb05 .av:hover     (active mouseover)
+        .jssorb05 .dn           (mousedown)
+        */
+        .jssorb05 {
+            position: absolute;
+        }
+        .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
+            position: absolute;
+            /* size of bullet elment */
+            width: 16px;
+            height: 16px;
+           /* background: url('img/b05.png') no-repeat;*/
+            overflow: hidden;
+            cursor: pointer;
+        }
+        .jssorb05 div { background-position: -7px -7px; }
+        .jssorb05 div:hover, .jssorb05 .av:hover { background-position: -37px -7px; }
+        .jssorb05 .av { background-position: -67px -7px; }
+        .jssorb05 .dn, .jssorb05 .dn:hover { background-position: -97px -7px; }
+
+        /* jssor slider arrow navigator skin 12 css */
+        /*
+        .jssora12l                  (normal)
+        .jssora12r                  (normal)
+        .jssora12l:hover            (normal mouseover)
+        .jssora12r:hover            (normal mouseover)
+        .jssora12l.jssora12ldn      (mousedown)
+        .jssora12r.jssora12rdn      (mousedown)
+        */
+        .jssora12l, .jssora12r {
+            display: block;
+            position: absolute;
+            /* size of arrow element */
+            width: 30px;
+            height: 46px;
+            cursor: pointer;
+           /* */
+            overflow: hidden;
+        }
+        .jssora12l { background-position: -16px -37px; }
+        .jssora12r { background-position: -75px -37px; }
+        .jssora12l:hover { background-position: -136px -37px; }
+        .jssora12r:hover { background-position: -195px -37px; }
+        .jssora12l.jssora12ldn { background-position: -256px -37px; }
+        .jssora12r.jssora12rdn { background-position: -315px -37px; }
+    </style>
+
 </head>
 <body>	
 	<?php
@@ -71,7 +172,7 @@
 												<div class="item active">
 									  <img src="http://placehold.it/1200x315" style = "opacity:0" alt="...">
 									  <div class="carousel-caption" style = "text-align:left;left:0px;width:100%">
-										  <h3 style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;><?php echo $topic;?></h3>
+										  <h3 style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;"><?php echo $topic;?></h3>
 										  <p><?php echo $string;?></p>
 										  
 									  </div>
@@ -83,7 +184,7 @@
 										<div class="item">
 									  <img src="http://placehold.it/1200x315" style = "opacity:0" alt="...">
 									  <div class="carousel-caption" style = "text-align:left;left:0px;width:100%">
-										  <h3 style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;><?php echo $topic;?></h3>
+										  <h3 style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;"><?php echo $topic;?></h3>
 										  <p><?php echo $string;?></p>
 										  
 									  </div>
@@ -135,7 +236,7 @@
 							}  	
 								?>
 								
-								<p style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;><?=$topic?></p>
+								<p style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;"><?=$topic?></p>
 								<p><?=$string?></p>
 								<?php
 								if($counter == 1){
@@ -171,7 +272,7 @@
 		
 	?>
 								
-								<p style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;><?=$topic?></p>
+								<p style = "font-weight: 600;font-size: 18px;text-decoration: none;color: #fff;"><?=$topic?></p>
 								<p>Starts on: <?=$start?> and Ends On: <?=$end?></p>
 								<?php 
 	
@@ -181,34 +282,38 @@
                     </div><!-- /.panel -->
 				</div>                
 				<div class="col-lg-4">
-					<div class="panel panel_news">
-                        <div class="panel-heading pull-right heading-news">
+		<div class="panel panel_news" style="padding-right: 4px; background-color: transparent;">
+                        <div class="panel-heading pull-center heading-news">
                             News Feeds
                         </div>
-						                        <div class = "communications" style = "padding:15px;position:absolute;bottom:0px">
-                         <?php
-		
-		
-		
-	foreach ($upcoming as $object ) {
-		$type=4;
-		$title1= $object->Topic;
-		$title= substr($title1, 0, 45);
-		$details= $object->details;
-		$id=$object->Blog_topic_id;
-		$start= $object->Start_Date;
-		$end=$object->End_Date;
-        $topic= anchor('home/single_events/'.$id.'/'.$type.'',''.$title.'');
-		
-	?>
-								
-								<p style = "font-weight: 600;font-size: 36px;text-decoration: none;color: #fff;width:299px;margin-bottom:160px;margin-left:39px">Coming Soon</p>
-								
-								<?php 
-	
-	}
-	?>
-	</div>
+
+        <div id="jssor_1" style="position: relative; margin: 0 auto; top: -3px; left: -2px; width: 350px; height: 600px; overflow: hidden; visibility: hidden;">
+					    <div class="example" data-u="slides" style="cursor: default; position: relative; top: -3px; left: -2px; width: 350px; height: 600px; overflow: hidden;">
+					            <div data-p="112.50" style="display: none;">
+					                <div class="post_results" id="post_results1" rss_num="5" rss_url="http://www.nation.co.ke/latestrss.rss">
+					            
+					                </div>
+					            </div>
+					            <div data-p="112.50" style="display: none;">
+					               <div class="post_results" id="post_results2" rss_num="5" rss_url="http://www.standardmedia.co.ke/rss/headlines.php">
+					           
+					                </div>
+					            </div>        
+        
+        				</div>
+        <!-- Bullet Navigator -->
+        <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
+            <!-- bullet navigator item prototype -->
+            <div data-u="prototype" style="width:16px;height:16px;"></div>
+        </div>
+        <!-- Arrow Navigator -->
+        <span data-u="arrowleft" class="jssora12l" style="top:0px;left:0px;width:30px;height:46px;" data-autocenter="2"></span>
+        <span data-u="arrowright" class="jssora12r" style="top:0px;right:0px;width:30px;height:46px;" data-autocenter="2"></span>
+    </div>
+    <script>
+        jssor_1_slider_init();
+    </script>
+
                     </div><!-- /.panel -->
 				</div><!-- /.panel -->
 								<div class="col-lg-4">
