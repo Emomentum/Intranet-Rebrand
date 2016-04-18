027f7f3335ca6e484a4a1b7e9a0b89666350b3d7
Emomentum <development.team@emomentum-interactive.com>
2016-04-18 15:11:10 +0300

    Afresh
    Removing previous data

--- application/views/includes/footer.php
+++
@@ -1,6 +0,0 @@
-	<footer>
-		<hr style = "margin-top:0px;height:1px;background-color:#3F66C5"/>	
-		<div style = "text-align:center;color:#818181;font-weight: 500;margin-bottom: 20px;">
-		<span>copyright eMomentum Interactive Systems Ltd 2016</span>
-		</div>
-	</footer>
